ee31f070d3f8c714bdd47a9d41c3db9e0e949231
SantosVicente <vidosantos45@gmail.com>
2023-09-12 10:39:28 -0300

    add prettier formatter

--- nlw-ia/src/App.tsx
+++ nlw-ia/src/App.tsx
@@ -1,10 +1,16 @@
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react"; 
-import { Button } from "./components/ui/button";
-import { Separator } from "./components/ui/separator";
-import { Textarea } from "./components/ui/textarea";
-import { Label } from "./components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
-import { Slider } from "./components/ui/slider";
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react';
+import { Button } from './components/ui/button';
+import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
+import { Label } from './components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
 export function App() {
   return (
@@ -19,10 +25,10 @@ export function App() {
 
           <Separator orientation="vertical" className="h-6" />
 
-          <Button variant={"outline"}>
-            <Github className="w-4 h-4 mr-2"/>
+          <Button variant={'outline'}>
+            <Github className="w-4 h-4 mr-2" />
             GitHub
-            </Button>
+          </Button>
         </div>
       </div>
 
@@ -30,34 +36,41 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea 
+            <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Inclua o prompt para a IA..." 
+              placeholder="Inclua o prompt para a IA..."
             />
-            <Textarea 
+            <Textarea
               className="resize-none p-4 leading-relaxed"
-              placeholder="Resultado gerado pela IA..." 
-              readOnly 
+              placeholder="Resultado gerado pela IA..."
+              readOnly
             />
           </div>
 
           <p className="text-sm text-muted-foreground">
-            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'${transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className="text-violet-400">{'${transcription}'}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo selecionado
           </p>
         </div>
-        
+
         <aside className="w-80 space-y-6">
           <form className="space-y-6">
-            <label 
+            <label
               htmlFor="video"
               className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10 hover:text-zinc-200 hover:border-zinc-600 transition-colors"
             >
-              <FileVideo className="w-4 h-4"/>
+              <FileVideo className="w-4 h-4" />
               Selecione um vídeo
             </label>
 
-            {/*sr-only faz o mesmo que display none, mas mantém ele no DOM, ou seja, ele continua clicável*/} 
-            <input type="file" id="video" accept="video/mp4" className="sr-only" /> 
+            {/*sr-only faz o mesmo que display none, mas mantém ele no DOM, ou seja, ele continua clicável*/}
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
 
             <Separator />
 
@@ -65,7 +78,7 @@ export function App() {
               <Label htmlFor="transcription-prompt">
                 Prompt de Transcrição
               </Label>
-              <Textarea 
+              <Textarea
                 id="transcription-prompt"
                 className="h-20 leading-relaxed resize-none"
                 placeholder="Inclua palavras-chave relacionadas ao vídeo separadas por vírgula ( , )"
@@ -83,13 +96,15 @@ export function App() {
           <form className="space-y-4">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select >
+              <Select>
                 <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
+                  <SelectValue placeholder="Selecione um prompt..." />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="titleYt">Título do YouTube</SelectItem>
-                  <SelectItem value="descriptionYt">Descrição do YouTube</SelectItem>
+                  <SelectItem value="descriptionYt">
+                    Descrição do YouTube
+                  </SelectItem>
                 </SelectContent>
               </Select>
             </div>
@@ -114,27 +129,23 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider
-                min={0}
-                max={1}
-                step={0.1}
-                className="w-full"
-              />
+              <Slider min={0} max={1} step={0.1} className="w-full" />
             </div>
 
             <span className="block text-xs text-muted-foreground italic">
-              Valores mais altos geram textos mais criativos, mas menos coerentes
+              Valores mais altos geram textos mais criativos, mas menos
+              coerentes
             </span>
 
             <Separator />
 
             <Button type="submit" className="w-full">
               Gerar Texto
-              <Wand2 className="w-4 h-4 ml-2"/>
+              <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
-      </main>  
+      </main>
     </div>
-  )
+  );
 }
